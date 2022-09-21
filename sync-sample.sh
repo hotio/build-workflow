ceb7cc6a12b2a1126bcd8a6b2036f9edf33ebf22
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-21 07:39:05 +0200

    cleanup repo

--- sync-sample.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while IFS= read -r repo; do
-    while IFS= read -r branch; do
-        echo "Updating ${repo}/${branch}..."
-    done < <(curl -u "${GITHUB_OWNER}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/${GITHUB_OWNER}/${repo}/branches" | jq -r '.[] | select(.name!="master") | .name')
-done < <(curl -u "${GITHUB_OWNER}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/users/${GITHUB_OWNER}/repos?per_page=1000" | jq -r '.[] | select(.topics[]=="docker-image") | .name')
